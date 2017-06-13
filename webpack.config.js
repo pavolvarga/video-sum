2115ec285b3b7d4700928da4257b454cbaff624f
Pavol Varga <pavol.varga@gmail.com>
2017-06-13 20:47:18 +0200

    Transcompile to current node version with babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,27 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+
     target: 'node',
     entry: './src/main.js',
     externals: [nodeExternals()],
     output: {
         filename: 'video-sum.js',
         path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    presets: [
+                        ['env', { targets: { node: 'current' } } ]
+                    ]
+                },
+            },
+        ],
     }
 };
